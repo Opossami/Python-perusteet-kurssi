8162d6b64fb518c4b7d413e8e68bd7c33946aa3f
Opossami <mies.smokissa@gmail.com>
2025-08-21 00:01:23 +0300

    Omia testijuttuja

---
+++ Omat_kikkailut/Testijuttuja.py
@@ -0,0 +1,10 @@
+# if else kikkailua
+
+print("Hei, kertoisitko nimesi?")
+name = input()
+if name == "Kiia":
+    print("Mee ny menee")
+elif name == "kiia":
+    print ("Mee ny menee")
+else:
+    print (f"Moi {name}")
