94a3ecf0e38b2796701bad380b98c37d32cbe325
Opossami <mies.smokissa@gmail.com>
2025-08-27 18:57:58 +0300

    vitonen on vaikein

---
+++ Tehtävä02_5.py
@@ -0,0 +1,14 @@
+#    Yksi leiviskä on 20 naulaa.
+#    Yksi naula on 32 luotia.
+#    Yksi luoti on 13,3 grammaa.
+print("Antaisitko esineen massan keskiaikaisten mittojen mukaan niin muutan sen kiloiksi!")
+
+leiviska = input("Kuinka monta leiviskää esine painaa?\n")
+leiviska = int(leiviska)
+
+naula = input("Kuinka monta naulaa esine painaa?\n")
+naula = int(naula)
+
+luoti = input("Kuinka monta luotia esine painaa?\n")
+luoti = int(luoti)
+
---
+++ Tehtävä02_6.py
@@ -0,0 +1,5 @@
+vastaus = input("Do you feel lucky?\n")
+import random
+print(random.randint(0, 999))
+
+print(random.randint(0, 9999))
