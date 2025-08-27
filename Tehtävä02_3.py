a6f8f7a39970b0d2e1ada27941699dfe1d293785
Opossami <mies.smokissa@gmail.com>
2025-08-27 18:02:13 +0300

    matematiikka on kivaa edelleenkin

---
+++ Tehtävä02_3.py
@@ -0,0 +1,7 @@
+kanta = input("Antaisitko suorakulmion kannan?\n")
+kanta = float(kanta)
+korkeus = input("Antaisitko vielä suorakulmion korkeuden?\n")
+korkeus = float(korkeus)
+
+print (f"Suorakulmion piiri on {kanta * 2 + korkeus * 2}")
+print (f"Suorakulmion pinta-ala on {kanta * korkeus}")
