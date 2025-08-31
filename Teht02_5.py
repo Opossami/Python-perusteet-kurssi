777947fc309978b7225dd86f8701ebe4f1f69ee6
Opossami <mies.smokissa@gmail.com>
2025-08-31 20:01:50 +0300

    Laskentatavan muutos

--- Teht02_5.py
+++ Teht02_5.py
@@ -12,3 +12,10 @@ naula = int(naula)
 luoti = input("Kuinka monta luotia esine painaa?\n")
 luoti = int(luoti)
 
+aksiviel = leiviska * 20 * 32 * 13,3
+aluan = naula * 32 * 13,3
+ituol = luoti * 13,3
+
+paino = ituol + aluan + aksiviel
+
+print(f"Antamasi esine painaa kiloissa {paino} grammaa.")
