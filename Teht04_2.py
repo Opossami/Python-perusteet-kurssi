debadda995f9ab5ff5e422e86145124e02b754e4
Opossami <mies.smokissa@gmail.com>
2025-09-28 20:26:28 +0300

    fix

--- Teht04_2.py
+++ Teht04_2.py
@@ -2,7 +2,7 @@ sentit = float(input("Anna tuumiksi muutettava senttimäärä: "))
 while sentit > 0:
     tuumat = sentit / 2.54
     print(f"{tuumat}.")
-    break
+    sentit = float(input("Anna tuumiksi muutettava senttimäärä: "))
 
-if sentit < 0:
+if sentit <= 0:
     print(f"Hyvästi.")
