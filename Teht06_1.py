9e641fe32282ba00ecfc7dc160ec753285a5fbc9
Opossami <mies.smokissa@gmail.com>
2025-11-02 18:49:28 +0200

    Uusien tehtävien alotus

---
+++ Teht06_1.py
@@ -0,0 +1,11 @@
+import random
+
+def noppa():
+    summa = random.randint(1, 6)
+        while summa < 6:
+            summa = random.randint(1,6)
+            print(summa)
+return summa
+
+noppa()
+
